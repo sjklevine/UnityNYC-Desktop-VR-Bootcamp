63145827da2771d98f92af00742df26e8276aec5
Sam Levine <samuel.levine@gmail.com>
2017-07-07 14:38:16 -0400

    Nodes...

--- UnityRoot/Assets/UnityNYC Bootcamp/Scripts/WristMenuMovementScript.cs
+++ UnityRoot/Assets/UnityNYC Bootcamp/Scripts/WristMenuMovementScript.cs
@@ -8,21 +8,23 @@ using VRTK;
 public class WristMenuMovementScript : MonoBehaviour {
 	public enum MovementTypes { None, StraightPointer, BezierPointer, RemoteBeam }
 	public MovementTypes currentMovementType = MovementTypes.None;
+	public bool restrictToNodes;
 
 	public GameObject movementButtonNone;
 	public GameObject movementButtonStraightPointer;
 	public GameObject movementButtonBezierPointer;
 	public GameObject movementButtonRemoteBeam;
+	public Toggle restrictToNodesToggle;
 
 	void OnEnable() {
-		// Because this prefab dies (or is at least inactivated) a whole lot, make sure the right button is re-pressed.
-		// Also wait a frame because Unity.
-		StartCoroutine(SelectCorrectButton());
+		// Because this prefab dies (or is at least inactivated) a whole lot, make sure the right buttons are pressed.
+		// Also, make this a coroutine, so we can wait a frame, because Unity.
+		StartCoroutine(UpdateUI());
 	}
 
 	public void OnButtonPress(string buttonString) {
 		//EventSystem.current.SetSelectedGameObject(null);
-		StartCoroutine(DeactivateEverything ());
+		StartCoroutine(DeactivateMovements ());
 		switch (buttonString) {
 		case "None":
 			currentMovementType = MovementTypes.None;
@@ -45,13 +47,32 @@ public class WristMenuMovementScript : MonoBehaviour {
 			currentMovementType = MovementTypes.RemoteBeam;
 			EventSystem.current.SetSelectedGameObject(movementButtonRemoteBeam);
 			// Just need to reactivate the beam.
-			GameObject.FindGameObjectWithTag("RemoteBeam").transform.GetChild(0).gameObject.SetActive (true);
+			GameObject.Find("/RemoteBeam").transform.GetChild(0).gameObject.SetActive (true);
 			break;
 		}
 	}
 
-	private IEnumerator SelectCorrectButton() {
+	public void OnToggleChange(bool newState) {
+		// Hide/Show the nodes!
+		foreach (Transform t in GameObject.Find("/DestinationPoints").transform) {
+			t.gameObject.SetActive (newState);
+		}
+
+		// Edit the VRTK target policy!  It's on the right hand, but the Remote uses it too.
+		VRTK_PolicyList policy = VRTK.VRTK_DeviceFinder.GetControllerRightHand ().GetComponent<VRTK_PolicyList> ();
+		if (newState) {
+			policy.identifiers = new List<string> { "MazeWalls", "Default" };
+		} else {
+			policy.identifiers = new List<string> { "MazeWalls", "DestinationPoints" };
+		}
+		restrictToNodes = newState;
+
+	}
+
+	private IEnumerator UpdateUI() {
 		yield return null; // Thanks, Unity
+
+		// Buttons!
 		EventSystem.current.SetSelectedGameObject(null);
 		switch (currentMovementType) {
 		case MovementTypes.None: EventSystem.current.SetSelectedGameObject(movementButtonNone); break;
@@ -59,12 +80,15 @@ public class WristMenuMovementScript : MonoBehaviour {
 		case MovementTypes.BezierPointer: EventSystem.current.SetSelectedGameObject(movementButtonBezierPointer); break;
 		case MovementTypes.RemoteBeam: EventSystem.current.SetSelectedGameObject(movementButtonRemoteBeam); break;
 		}
+
+		// Toggle!
+		restrictToNodesToggle.isOn = this.restrictToNodes;			
 	}
 
-	private IEnumerator DeactivateEverything() {
+	private IEnumerator DeactivateMovements() {
 		// Disable RemoteBeam by actually setting the Remote to inactive!
 		// This line sucks because Unity doesn't let you find inactive gameobjects easily, and we can't save a reference.
-		GameObject.FindGameObjectWithTag("RemoteBeam").transform.GetChild(0).gameObject.SetActive (false);
+		GameObject.Find("/RemoteBeam").transform.GetChild(0).gameObject.SetActive (false);
 
 		// Just killing the reference to Renderers in the Pointer stops the StraightPointer & BezierPointer methods!
 		// Right hand might not be available, so wait until it is.
--- UnityRoot/Assets/VRTK/Examples/ExampleResources/Scripts/Remote_Beam.cs
+++ UnityRoot/Assets/VRTK/Examples/ExampleResources/Scripts/Remote_Beam.cs
@@ -1,14 +1,23 @@
 ﻿namespace VRTK.Examples
 {
+	using System.Collections;
     using UnityEngine;
 
     public class Remote_Beam : MonoBehaviour
     {
+		public bool startDisabled;
+
         private Vector2 touchAxis;
         private float rotationSpeed = 180f;
         private float currentYaw;
         private float currentPitch;
 
+		void Start() {
+			// Have to use a coroutine for this, otherwise VRTK won't teleport properly...
+			if (startDisabled) {
+				StartCoroutine (WaitThenDisable ());
+			}
+		}
         public void SetTouchAxis(Vector2 data)
         {
             touchAxis = data;
@@ -20,5 +29,10 @@
             currentPitch += touchAxis.x * rotationSpeed * Time.deltaTime;
             transform.localRotation = Quaternion.AngleAxis(currentPitch, Vector3.up) * Quaternion.AngleAxis(currentYaw, Vector3.left);
         }
+
+		private IEnumerator WaitThenDisable() {
+			yield return new WaitForSeconds (0.5f);
+			this.gameObject.SetActive (false);
+		}
     }
 }
