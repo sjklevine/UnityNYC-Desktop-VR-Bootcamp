5a6a7fba819a4affd960eb7065c2f4ef57bad26c
Sam Levine <samuel.levine@gmail.com>
2017-07-28 18:52:32 -0400

    Uploading the post-class freeform dev work done @ Babycastles, with just a touch of cleanup.

---
+++ UnityRoot/Assets/UnityNYC Bootcamp/Babycastles/BlockSpawnerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawnerScript : MonoBehaviour {
+
+    public GameObject[] blockPrefabs;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("SPAWNING BLOCK");
+            SpawnNewBlock();
+            SpawnNewBlock();
+            SpawnNewBlock();
+            SpawnNewBlock();
+            SpawnNewBlock();
+            SpawnNewBlock();
+            SpawnNewBlock();
+            SpawnNewBlock();
+        }
+    }
+    public void SpawnNewBlock()
+    {
+        // Spawn the block right here (and let rigidbody physics toss it)
+        int whichPrefab = Random.Range(0, blockPrefabs.Length-1);
+        GameObject.Instantiate(blockPrefabs[whichPrefab], this.transform.position, this.transform.rotation);
+    }
+}
---
+++ UnityRoot/Assets/UnityNYC Bootcamp/Babycastles/BlockStickingScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class BlockStickingScript : MonoBehaviour {
+
+    private VRTK_InteractableObject interactionScript;
+
+	void Start () {
+        interactionScript = this.GetComponent<VRTK_InteractableObject>();		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("ON COLLISION ENTER CALLED, hit " + collision.gameObject.name + ", isgrabbed = " + interactionScript.IsGrabbed());
+        if (interactionScript.IsGrabbed())
+        {
+            // Don't stick to the floor!
+            if (collision.gameObject.tag.Equals("Floor"))
+            {
+                return;
+            }
+            GameObject.Destroy(collision.gameObject.GetComponent<Rigidbody>());
+            GameObject.Destroy(collision.gameObject.GetComponent<VRTK_InteractableObject>());
+            collision.gameObject.transform.SetParent(this.transform);
+        }
+    }
+}
